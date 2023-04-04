212aa1da8e5abd4abeadcf673c1404f271253c76
XeniyaZ <kseniya10081989@gmail.com>
2023-04-04 11:02:42 -0500

    deployment prep and refine

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-const btn = document.querySelector('#input-btn')
-btn.addEventListener('click', getClicked)
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 let ulEl = document.getElementById('ul-el')
+
+const btn = document.querySelector('#input-btn')
+btn.addEventListener('click', getClicked)
+const deleteBtn = document.querySelector('#delete-btn')
+
+deleteBtn.addEventListener("click", function() {
+    localStorage.clear()
+    myLeads = []
+    renderList()
+})
+
+
+const leadsFromStorage = JSON.parse(localStorage.getItem ('myLeads'))
+
+if(leadsFromStorage){
+    myLeads = leadsFromStorage
+    renderList()
+}
 
 
 function getClicked(){
      myLeads.push(inputEl.value)
+     inputEl.value = ''
+localStorage.setItem('myLeads', JSON.stringify(myLeads))
+renderList()
+console.log( localStorage.getItem("myLeads") )
+
 }
 
-for (let i = 0;  i< myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
- 
-}
 
+function renderList(){
+
+let listItems = ''
+for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li>
+        <a target = '_blank' href='${myLeads[i]}'>${myLeads[i]}
+        </a>
+    </li>
+    `
+    }
+
+ulEl.innerHTML = listItems
+
+}
