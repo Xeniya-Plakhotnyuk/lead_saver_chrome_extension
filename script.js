a77ef63a634b5144679a900841e87f1cc4a82c12
XeniyaZ <kseniya10081989@gmail.com>
2023-04-04 16:14:07 -0500

    complete script of the extension

--- script.js
+++ script.js
@@ -10,35 +10,44 @@ const deleteBtn = document.querySelector('#delete-btn')
 deleteBtn.addEventListener("click", function() {
     localStorage.clear()
     myLeads = []
-    renderList()
+    render(myLeads)
 })
 
+
+const saveTab = document.querySelector('#save-btn')
+saveTab.addEventListener('click', function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 const leadsFromStorage = JSON.parse(localStorage.getItem ('myLeads'))
 
 if(leadsFromStorage){
     myLeads = leadsFromStorage
-    renderList()
+    render(myLeads)
 }
 
 
 function getClicked(){
-     myLeads.push(inputEl.value)
-     inputEl.value = ''
+myLeads.push(inputEl.value)
+inputEl.value = ''
 localStorage.setItem('myLeads', JSON.stringify(myLeads))
-renderList()
+render(myLeads)
 console.log( localStorage.getItem("myLeads") )
 
 }
 
 
-function renderList(){
+function render(leads){
 
 let listItems = ''
-for (let i = 0; i < myLeads.length; i++) {
+for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-        <a target = '_blank' href='${myLeads[i]}'>${myLeads[i]}
+        <a target = '_blank' href='${leads[i]}'>${leads[i]}
         </a>
     </li>
     `
